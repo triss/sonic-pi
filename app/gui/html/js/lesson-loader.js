487007e53079049b18e91bcc34685c43f75e470f
Tristan Strange <tristan.strange@gmail.com>
2014-01-09 00:13:42 +0000

    Forgot to add lessons

---
+++ app/gui/html/js/lesson-loader.js
@@ -0,0 +1,10 @@
+function httpGet(theUrl) {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open("GET", theUrl, false);
+	xmlHttp.send(null);
+	return xmlHttp.responseText;
+}
+
+function loadLesson(url) {
+	document.getElementById("lessons").innerHTML = httpGet(url);
+}
